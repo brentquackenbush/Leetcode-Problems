937a4bf3761498b662d76828969929c7fea881f5
Brent Quackenbush <brentprogramming@gmail.com>
2022-11-28 11:28:24 -0600

    Able to find the duplicate number in an array without changing the array and doing so in a linear time.

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,35 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+Follow up:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?
+ */
+public class FindDuplicateNumber {
+    public static void main(String[] args) {
+        int[] nums = {3,1,3,4,2};
+        System.out.println(findDuplicate(nums));
+    }
+    public static int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        slow = 0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
+
+/*
+Time complexity: O(n)
+Space Complexity: O(1)
+ */
